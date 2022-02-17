ee4656756c9c84a9f556fc69589837e8a99d54e4
dalbeertws <dalbeer.tws@tekkiwebsolutions.com>
2022-02-17 10:31:34 +0530

    Deployed through PostgreSQL

--- todo/settings.py
+++ todo/settings.py
@@ -78,12 +78,12 @@ WSGI_APPLICATION = 'todo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.mysql',
@@ -92,7 +92,16 @@ DATABASES = {
 #         },
 #     }
 # }
-
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'blog', 
+        'USER': 'postgres', 
+        'PASSWORD': 'tws',
+        'HOST': '127.0.0.1', 
+        'PORT': '5432',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
